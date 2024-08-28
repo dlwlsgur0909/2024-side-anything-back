f414b9e8f015272f6304855fdf255929bca314db
dlwlsgur0909 <dlwlsgur0909@naver.com>
2024-08-28 15:00:38 +0900

    feat: SecurityConfig 기본 설정 및 AuthEntryPoint 추가

---
+++ back/src/main/java/com/side/anything/back/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.side.anything.back.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private AuthEntryPoint authEntryPoint;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPoint))
+                .authorizeHttpRequests(request ->
+                    request
+                            .requestMatchers("/", "/auth/login", "/auth/join").permitAll()
+                            .anyRequest().authenticated()
+                )
+                .build();
+    }
+}
