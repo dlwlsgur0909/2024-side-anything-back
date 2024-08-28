94ae240f77d509219b3fca100996bb0ac66b26f8
dlwlsgur0909 <dlwlsgur0909@naver.com>
2024-08-28 15:50:07 +0900

    feat: JwtUtil 및 yml 설정 파일 추가

--- back/src/main/java/com/side/anything/back/jwt/JwtUtil.java
+++ back/src/main/java/com/side/anything/back/jwt/JwtUtil.java
@@ -1,2 +1,68 @@
-package com.side.anything.back.jwt;public class JwtUtil {
+package com.side.anything.back.jwt;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key secretKey;
+
+    public JwtUtil(@Value("spring.jwt.secret") String secret) {
+        secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String createJwt(String username, String role, Long expiration) {
+
+        return Jwts.builder()
+                .claim("username", username)
+                .claim("role", role)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public String getUsername(String token) {
+
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody()
+                .get("username", String.class);
+    }
+
+    public String getRole(String token) {
+
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody()
+                .get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(secretKey)
+                .build()
+                .parseClaimsJwt(token)
+                .getBody()
+                .getExpiration()
+                .before(new Date());
+    }
+
+
 }
