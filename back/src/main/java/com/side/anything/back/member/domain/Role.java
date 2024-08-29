30e9797a115613d3ef92171701bfe76b857c38cb
dlwlsgur0909 <dlwlsgur0909@naver.com>
2024-08-29 10:59:22 +0900

    feat: Member 엔티티, CustomUserDetails, Audit 관련 파일 추가

---
+++ back/src/main/java/com/side/anything/back/member/domain/Role.java
@@ -0,0 +1,22 @@
+package com.side.anything.back.member;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public enum Role {
+
+    USER("ROLE_USER", "회원"),
+    ADMIN("ROLE_ADMIN", "관리자")
+    ;
+
+    private final String key;
+    private final String title;
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
---
+++ back/src/main/java/com/side/anything/back/security/CustomUserDetails.java
@@ -0,0 +1,43 @@
+package com.side.anything.back.member;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Getter
+@RequiredArgsConstructor
+@Builder
+public class CustomUserDetails implements UserDetails {
+
+    private final Member member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return member.getRole().getKey();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+}
