15b5278588362a0048d0e2af634832d3c61e7eef
dlwlsgur0909 <dlwlsgur0909@naver.com>
2024-08-29 14:01:03 +0900

    feat: 회원가입 기능 구현

--- back/src/main/java/com/side/anything/back/config/SecurityConfig.java
+++ back/src/main/java/com/side/anything/back/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.side.anything.back.config;
 
+import com.side.anything.back.filter.JwtFilter;
+import com.side.anything.back.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,14 +10,25 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
 
     @Autowired
     private AuthEntryPoint authEntryPoint;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -31,6 +44,7 @@ public class SecurityConfig {
                             .requestMatchers("/", "/auth/login", "/auth/join").permitAll()
                             .anyRequest().authenticated()
                 )
+                .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
