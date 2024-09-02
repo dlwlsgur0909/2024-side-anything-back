82f52cecfd37557156508b2336b9474bc623b090
dlwlsgur0909 <dlwlsgur0909@naver.com>
2024-09-02 16:45:25 +0900

    feat: 메일 발송 에러 처리

--- back/src/main/java/com/side/anything/back/exception/GlobalExceptionHandler.java
+++ back/src/main/java/com/side/anything/back/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.side.anything.back.exception;
 
+import jakarta.mail.SendFailedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailSendException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -19,6 +21,20 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .build();
+    }
+
+    @ExceptionHandler({
+            MailSendException.class
+    })
+    public ResponseEntity<?> mailExceptionHandler(Exception e) {
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(BasicExceptionEntity.builder()
+                        .errorCode("500")
+                        .errorMessage("메일이 정상적으로 발송되지 않았습니다.")
+                        .build()
+                );
     }
 
     @ExceptionHandler({
