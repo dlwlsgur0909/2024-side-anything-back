b931009c052f3299cc29010df9866ee4acd93686
dlwlsgur0909 <dlwlsgur0909@naver.com>
2024-09-20 10:06:47 +0900

    feat: OAuth2Response에서 attribute를 꺼내 담을 CustomDTO 추가

--- back/src/main/java/com/side/anything/back/security/dto/response/CustomUserDTO.java
+++ back/src/main/java/com/side/anything/back/security/dto/response/CustomUserDTO.java
@@ -1,2 +1,17 @@
-package com.side.anything.back.security.dto.response;public class CustomUserDTO {
+package com.side.anything.back.security.dto.response;
+
+import com.side.anything.back.member.domain.Role;
+import lombok.*;
+
+@Getter @ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomUserDTO {
+
+    private String username;
+    private String name;
+    private String email;
+    private Role role;
+
 }
