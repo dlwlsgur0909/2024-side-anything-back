9b4c22ce06d3b3610c68a82f5144d1587efcab5e
dlwlsgur0909 <dlwlsgur0909@naver.com>
2025-05-21 16:47:32 +0900

    refactor: PortfolioDetailResponse 작성자 정보 추가

--- back/src/main/java/com/side/anything/back/portfolio/dto/response/PortfolioDetailResponse.java
+++ back/src/main/java/com/side/anything/back/portfolio/dto/response/PortfolioDetailResponse.java
@@ -1,5 +1,6 @@
 package com.side.anything.back.portfolio.dto.response;
 
+import com.side.anything.back.member.domain.Member;
 import com.side.anything.back.portfolio.domain.Portfolio;
 import lombok.Getter;
 
@@ -11,6 +12,8 @@ public class PortfolioDetailResponse {
     private String portfolioContent;
     private String portfolioUrl;
     private Boolean isPublic;
+    private Long memberId;
+    private String memberName;
 
     public PortfolioDetailResponse(Portfolio portfolio) {
         this.portfolioId = portfolio.getId();
@@ -18,5 +21,7 @@ public class PortfolioDetailResponse {
         this.portfolioContent = portfolio.getContent();
         this.portfolioUrl = portfolio.getUrl();
         this.isPublic = portfolio.getIsPublic();
+        this.memberId = portfolio.getMember().getId();
+        this.memberName = portfolio.getMember().getName();
     }
 }
