1b2996387e20fa9eb3f8dd669e9dbfa532fedfeb
dlwlsgur0909 <dlwlsgur0909@naver.com>
2025-06-24 09:46:39 +0900

    refactor: CompanionApplicationListResponse isCancelable 필드 값 설정 로직 변경

--- back/src/main/java/com/side/anything/back/companion/dto/response/CompanionApplicationListResponse.java
+++ back/src/main/java/com/side/anything/back/companion/dto/response/CompanionApplicationListResponse.java
@@ -2,6 +2,7 @@ package com.side.anything.back.companion.dto.response;
 
 import com.side.anything.back.companion.entity.CompanionApplication;
 import com.side.anything.back.companion.entity.CompanionApplicationStatus;
+import com.side.anything.back.companion.entity.CompanionPostStatus;
 import lombok.Getter;
 
 import java.util.List;
@@ -33,14 +34,16 @@ public class CompanionApplicationListResponse {
         public CompanionApplicationResponse(CompanionApplication companionApplication) {
             this.applicationId = companionApplication.getId();
             this.applicationStatus = companionApplication.getStatus().getDescription();
-            this.isCancelable = List.of(
-                        CompanionApplicationStatus.PENDING, CompanionApplicationStatus.APPROVED
-                    )
-                    .contains(companionApplication.getStatus());
+            this.isCancelable = checkIsCancelable(companionApplication.getStatus(), companionApplication.getCompanionPost().getStatus());
             this.postId = companionApplication.getCompanionPost().getId();
             this.postTitle = companionApplication.getCompanionPost().getTitle();
             this.postLocation = companionApplication.getCompanionPost().getLocation();
             this.postStatus = companionApplication.getCompanionPost().getStatus().getDescription();
+        }
+
+        private Boolean checkIsCancelable(CompanionApplicationStatus applicationStatus, CompanionPostStatus postStatus) {
+            return List.of(CompanionApplicationStatus.PENDING, CompanionApplicationStatus.APPROVED).contains(applicationStatus)
+                    && postStatus == CompanionPostStatus.OPEN;
         }
     }
 
