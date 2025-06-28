d171862aaf9e8e45944bf34a7502963dbe0f7534
LJH09 <dlwlsgur0909@naver.com>
2025-06-28 17:59:25 +0900

    refactor: ChatController 수정

--- back/src/main/java/com/side/anything/back/chat/controller/ChatController.java
+++ back/src/main/java/com/side/anything/back/chat/controller/ChatController.java
@@ -5,15 +5,18 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class ChatController {
 
-    @MessageMapping("/chat.{roomId}") // 클라이언트가 서버로 메세지를 보낼 때의 경로
-    @SendTo("/sub/chat/{roodId}")
+    @MessageMapping("/chat/{roomId}") // 클라이언트가 서버로 메세지를 보낼 때의 경로
+    @SendTo("/sub/chat/{roomId}")
     public ChatMessageRequest sendMessage(@DestinationVariable Long roomId, @Payload ChatMessageRequest request) {
+        
         // 서비스 로직 호출 필요
+
+        return request;
     }
 
 }
