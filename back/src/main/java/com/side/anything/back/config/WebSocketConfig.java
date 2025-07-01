5446d6e97c75e2ee6a0b1c7e6fc4d71bdc8e5c93
dlwlsgur0909 <dlwlsgur0909@naver.com>
2025-07-01 10:46:50 +0900

    feat: StompJwtChannelInterceptor 추가 및 등록

--- back/src/main/java/com/side/anything/back/config/WebSocketConfig.java
+++ back/src/main/java/com/side/anything/back/config/WebSocketConfig.java
@@ -1,6 +1,9 @@
 package com.side.anything.back.config;
 
+import com.side.anything.back.security.stomp.StompJwtChannelInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,7 +11,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker // STOMP 기반 WebSocket 활성화
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    private final StompJwtChannelInterceptor stompJwtChannelInterceptor;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
@@ -21,5 +27,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/sub"); // 서버가 클라이언트로 메세지를 보낼 때 사용하는 prefix
         registry.setApplicationDestinationPrefixes("/pub"); // 클라이언트가 서버로 메세지를 보낼 때 사용하는 prefix
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompJwtChannelInterceptor);
     }
 }
---
+++ back/src/main/java/com/side/anything/back/security/stomp/StompJwtChannelInterceptor.java
@@ -0,0 +1,57 @@
+package com.side.anything.back.security.stomp;
+
+import com.side.anything.back.exception.BasicExceptionEnum;
+import com.side.anything.back.exception.CustomException;
+import com.side.anything.back.security.jwt.JwtUtil;
+import com.side.anything.back.security.jwt.TokenInfo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class StompJwtChannelInterceptor implements ChannelInterceptor {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+
+        if(StompCommand.CONNECT.equals(accessor.getCommand())) {
+
+            String authorization = accessor.getFirstNativeHeader(HttpHeaders.AUTHORIZATION);
+
+            if(authorization == null || !authorization.startsWith("Bearer ")) {
+                throw new CustomException(BasicExceptionEnum.UNAUTHORIZED, "Invalid Authorization");
+            }
+
+            String token = authorization.substring(7);
+
+            if(jwtUtil.isInvalid(token)) {
+                throw new CustomException(BasicExceptionEnum.UNAUTHORIZED, "Invalid JWT");
+            }
+
+            TokenInfo tokenInfo = jwtUtil.parseToken(token);
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    tokenInfo, null, List.of(new SimpleGrantedAuthority(tokenInfo.getRole().name()))
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        return message;
+    }
+}
