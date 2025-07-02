455d00fe3f9c078f27c48e27f7eb00bfca32aa62
dlwlsgur0909 <dlwlsgur0909@naver.com>
2025-07-02 21:06:33 +0900

    refactor: ChatMessage 상속 관계 수정(BaseEntity -> BaseTimeEntity)

--- back/src/main/java/com/side/anything/back/chat/entity/ChatMessage.java
+++ back/src/main/java/com/side/anything/back/chat/entity/ChatMessage.java
@@ -1,16 +1,22 @@
 package com.side.anything.back.chat.entity;
 
-import com.side.anything.back.base.BaseEntity;
+import com.side.anything.back.base.BaseTimeEntity;
 import com.side.anything.back.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ChatMessage extends BaseEntity {
+public class ChatMessage extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
