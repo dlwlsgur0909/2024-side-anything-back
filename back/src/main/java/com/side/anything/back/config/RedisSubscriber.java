e0637881730f199d4cea751f420d4160d7022b23
dlwlsgur0909 <dlwlsgur0909@naver.com>
2025-07-11 12:55:30 +0900

    refactor: RedisSubscriber 빈 타입 변경 SimpMessagingTemplate(구현체) -> SimpMessageSendingOperations(인터페이스)

--- back/src/main/java/com/side/anything/back/config/RedisSubscriber.java
+++ back/src/main/java/com/side/anything/back/config/RedisSubscriber.java
@@ -9,6 +9,7 @@ import com.side.anything.back.exception.CustomException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
@@ -19,7 +20,7 @@ import java.nio.charset.StandardCharsets;
 public class RedisSubscriber implements MessageListener {
 
     private final ObjectMapper objectMapper;
-    private final SimpMessagingTemplate messagingTemplate; // STOMP 프로토콜 기반으로 WebSocket 클라이언트에게 메세지를 전송
+    private final SimpMessageSendingOperations messagingTemplate; // STOMP 프로토콜 기반으로 WebSocket 클라이언트에게 메세지를 전송
 
     // Redis Pub/Sub을 통해 들어온 메세지를 받아서 처리하는 콜백 메서드
     @Override
